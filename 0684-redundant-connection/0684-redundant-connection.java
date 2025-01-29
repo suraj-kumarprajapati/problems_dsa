15ac0dbd95788b8ddd5f82593ea8131791aefa53
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-01-29 20:07:42 +0530

    updated 0684-redundant-connection.java

--- 0684-redundant-connection/0684-redundant-connection.java
+++ 0684-redundant-connection/0684-redundant-connection.java
@@ -25,7 +25,8 @@ class Solution {
             int u = edge[0];
             int v = edge[1];
 
-            // remove this edge from the adj list and check if cycle exists of not
+            
+            // remove this edge from the adj list and check if you can visit all the nodes or not
             adj.get(u).remove(Integer.valueOf(v));
             adj.get(v).remove(Integer.valueOf(u));
 
