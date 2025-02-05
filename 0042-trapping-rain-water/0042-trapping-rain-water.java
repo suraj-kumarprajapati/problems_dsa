2e2186df839bb50a34b1279cf1585c5632ef8fcf
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-02-05 20:05:27 +0530

    Time: 1 ms (64.92%), Space: 46.9 MB (13.15%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.java
+++ 0042-trapping-rain-water/0042-trapping-rain-water.java
@@ -1,8 +1,41 @@
 class Solution {
     public int trap(int[] height) {
-        return sol1(height);
+        // return sol1(height);
+
+        return sol2(height);
     }
 
+    public int sol2(int[] height) {
+        int n = height.length;
+        int vol = 0;
+
+        int i = 0;
+        int j = n-1;
+        int leftMax = 0;
+        int rightMax = 0;
+        while(i < j) {
+            if(height[i] <= height[j]) {
+                if( leftMax > height[i] ) {
+                    vol += (leftMax - height[i]);
+                }
+
+                leftMax = Math.max(leftMax, height[i]);
+                i += 1;
+            }
+            else {
+                if(rightMax > height[j]) {
+                    vol += (rightMax - height[j]);
+                }
+
+                rightMax = Math.max(rightMax, height[j]);
+                j -= 1;
+            }
+        }
+
+        return vol;
+    }
+
+    // time -> O(n)
     public int sol1(int[] height) {
         int n = height.length;
 
