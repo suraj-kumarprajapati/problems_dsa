b7975bb84f8f6e345c557279bac6761c2b2a2c0b
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-02-06 17:35:21 +0530

    Update 1726-tuple-with-same-product.java

--- 1726-tuple-with-same-product/1726-tuple-with-same-product.java
+++ 1726-tuple-with-same-product/1726-tuple-with-same-product.java
@@ -21,13 +21,6 @@ class Solution {
         return totalTuples;
     }
 
-    public int getFact(int n) {
-        int res = 1;
-        for(int i=1; i<=n; i++) {
-            res *= i;
-        }
-
-        return res;
-    }
+   
 
 }
