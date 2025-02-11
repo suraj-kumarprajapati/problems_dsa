22de3f3e70fac562b6419b8b17eaf03c9a452f4a
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-02-11 08:14:58 +0530

    Time: 13 ms (9.71%), Space: 45 MB (15.02%) - LeetHub

---
+++ 1910-remove-all-occurrences-of-a-substring/1910-remove-all-occurrences-of-a-substring.java
@@ -0,0 +1,49 @@
+class Solution {
+    public String removeOccurrences(String s, String part) {
+        return removeOccurencesUsingStack(s, part);
+    }
+
+    public String removeOccurencesUsingStack(String s, String part) {
+        int n = s.length();
+
+        Stack<Character> st = new Stack<>();
+        for(int i=0; i<n; i++) {
+            char c = s.charAt(i);
+
+            st.push(c);
+
+            if( st.size() >= part.length() && doesMatch(st, part) ) {
+                int j = 0;
+                while(j < part.length()) {
+                    st.pop();
+                    j += 1;
+                }
+            }
+        }
+
+
+        StringBuilder sb = new StringBuilder();
+        while(!st.isEmpty()) {
+            sb.append(st.pop());
+        }
+
+        sb.reverse();
+
+        return sb.toString();
+    }
+
+    public boolean doesMatch(Stack<Character> st, String part) {
+        int m = part.length();
+        Stack<Character> temp = new Stack<>();
+        temp.addAll(st);
+
+        for(int i=m-1; i>=0; i--) {
+            if(!temp.isEmpty() && temp.peek() != part.charAt(i)) {
+                return false;
+            }
+            temp.pop();
+        }
+
+        return true;
+    }
+}
