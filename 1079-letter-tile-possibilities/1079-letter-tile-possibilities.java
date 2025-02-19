10c0948cf3523fd9e55a5b99319aa79cf7691c99
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-02-19 07:56:38 +0530

    Time: 7 ms (47.16%), Space: 43.1 MB (47.6%) - LeetHub

--- 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
+++ 1079-letter-tile-possibilities/1079-letter-tile-possibilities.java
@@ -1,8 +1,84 @@
 class Solution {
+
+    int[] fact = new int[8];
+
     public int numTilePossibilities(String tiles) {
-        return sol1(tiles);
+        // return sol1(tiles);
+
+        calcFact();
+
+        return sol2(tiles);
     }
 
+
+    public int sol2(String tiles) {
+        int n = tiles.length();
+
+        char[] tilesArr = tiles.toCharArray();
+        Arrays.sort(tilesArr);
+        
+        Set<String> seq = new HashSet<>();
+
+        return genSeq(0, "", tilesArr, seq);
+    }
+
+    public int genSeq(int ind, String str, char[] tilesArr, Set<String> seq) {
+        int n = tilesArr.length;
+
+        // base case
+        if(ind == n) {
+            if(str.equals("") || seq.contains(str)) {
+                return 0;
+            }
+            else {
+                seq.add(str);
+                int perm = getPermutation(str);
+                return perm;
+            }
+        }
+
+        // two options -> take this char or don't take it
+        // 1. don't take
+        int res1 = genSeq(ind+1, str, tilesArr, seq);
+        // 2. take 
+        int res2 = genSeq(ind+1, str + tilesArr[ind], tilesArr, seq);
+        return res1 + res2;
+    }
+
+    public void calcFact() {
+        int n = fact.length;
+        fact[0] = 1;
+        fact[1] = 1;
+
+        for(int i=2; i<n; i++) {
+            fact[i] = i * fact[i-1];
+        }
+    }
+
+
+    public int getPermutation(String str) {
+        int n = str.length();
+        int perm = fact[n];
+        Map<Character, Integer> freq = new HashMap<>();
+        for(int i = 0; i<n; i++) {
+            char ch = str.charAt(i);
+            freq.put(ch, freq.getOrDefault(ch, 0) + 1);
+        }
+
+        for(Map.Entry<Character, Integer> it : freq.entrySet()) {
+            int f = it.getValue();
+            perm = perm / fact[f];
+        }
+
+        return perm;
+    }
+
+    
+
+
+
+    // time -> O(n! * n) : n! for different sequence generation and n for string concatenation
+    // space -> O(n! * n) , n! -> no. of sequences, n -> length of each sequence
     public int sol1(String tiles) {
         int n = tiles.length();
         Set<String> seq = new HashSet<>();
@@ -16,7 +92,7 @@ class Solution {
     public void generateSequences(String tiles, String str, 
     Set<String> seq, boolean[] used) {
         int n = tiles.length();
-
+ 
         if(!str.equals(""))
             seq.add(str);
 
