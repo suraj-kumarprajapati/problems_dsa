b454af68d75c34d9a6603efaabeaf048d0ca4cc0
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-02-21 22:06:00 +0530

    Time: 2 ms (50.65%), Space: 41.2 MB (98.44%) - LeetHub

---
+++ 1980-find-unique-binary-string/1980-find-unique-binary-string.java
@@ -0,0 +1,68 @@
+class Solution {
+    public String findDifferentBinaryString(String[] nums) {
+        return sol1(nums);
+    }
+
+    public String sol1(String[] nums) {
+        int n = nums.length;
+
+        Set<Integer> uniqueNumbers = new HashSet<>();   
+        for(String num : nums) {
+            int dec = getDecimal(num);
+            uniqueNumbers.add(dec);
+        }
+
+        int res = 0;
+        for(int i=0; i<Math.pow(2, n); i++) {
+            if(!uniqueNumbers.contains(i)) {
+                res = i;
+                break;
+            }
+        }
+
+        return getBinary(res, n);
+    }
+
+    public String getBinary(int x, int n) {
+        StringBuilder str = new StringBuilder();
+
+        // base  case
+        if(x == 0) {
+            for(int i=0; i<n; i++) {
+                str.append(0);
+            }
+
+            return str.toString();
+        }
+
+        while(x != 0) {
+            int rem = (x & 1);
+            str.append(rem);
+            x = x >> 1;
+        }
+
+        while(str.length() != n) {
+            str.append(0);
+        }
+
+        str.reverse();
+        return str.toString();
+    }
+
+
+    public int getDecimal(String num) {
+        int res = 0;
+        int n = num.length();
+
+        int mul = 1;
+        for(int i=n-1; i>=0; i--) {
+            char ch = num.charAt(i);
+            int bit = (int) (ch - '0');
+            res = res + bit*mul;
+            mul *= 2;
+        }
+
+        return res;
+    }
+
+}
