8d6814eb9d9c8e922ed1a75858f4c3c41d5c8329
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-02-22 22:35:52 +0530

    Time: 1 ms (97.85%), Space: 41.7 MB (63.82%) - LeetHub

--- 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
+++ 1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n/1415-the-k-th-lexicographical-string-of-all-happy-strings-of-length-n.java
@@ -4,7 +4,72 @@ class Solution {
     int k;
     int count = 0;
 
+
+
     public String getHappyString(int n, int k) {
+        // return sol1(n, k);
+
+        return sol2(n, k);
+    }
+    
+    // using cominatorics
+    public String sol2(int n, int k) {
+
+        int total = 3 * (1 << (n-1));
+        if(k > total)
+            return "";
+        
+        char[] s = new char[n];
+
+        int startA = 1;
+        int startB = (1<<(n-1)) + 1;
+        int startC = 2*(1<<(n-1)) + 1;
+
+        Map<Character, Character> nextSmallest = new HashMap<>();
+        Map<Character, Character> nextGreatest = new HashMap<>();
+        nextSmallest.put('a', 'b');
+        nextSmallest.put('b', 'a');
+        nextSmallest.put('c', 'a');
+
+        nextGreatest.put('a', 'c');
+        nextGreatest.put('b', 'c');
+        nextGreatest.put('c', 'b');
+
+
+        if(k < startB) {
+            s[0] = 'a';
+            k = k - startA;
+        }
+        else if(k < startC) {
+            s[0] = 'b';
+            k = k - startB;
+        }
+        else {
+            s[0] = 'c';
+            k = k - startC;
+        }
+
+
+        for(int i=1; i<n; i++) {
+            int mid = (1 <<(n-i-1));
+
+            if(k < mid) {
+                s[i] = nextSmallest.get(s[i-1]);
+            }
+            else {
+                s[i] = nextGreatest.get(s[i-1]);
+                k = k-mid;
+            }
+
+            
+        }
+
+        return new String(s);
+    }
+
+
+    // using recursion + backtracking
+    public String sol1(int n, int k) {
         this.n = n;
         this.k = k;
         this.set = new char[] {'a', 'b', 'c'};
