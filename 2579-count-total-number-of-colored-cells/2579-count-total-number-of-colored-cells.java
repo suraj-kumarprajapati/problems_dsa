f9656562631cfd467f2c753bad29a35099730d1b
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-03-05 23:11:18 +0530

    Time: 3 ms (42.21%), Space: 40.7 MB (37.3%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
@@ -1,8 +1,15 @@
 class Solution {
     public long coloredCells(int n) {
 
-        long gridSize = 2*n-1;
-        long halfGridSize = gridSize / 2 + 1;
-        return gridSize*gridSize - 2*(halfGridSize*halfGridSize - halfGridSize);
+        long ans = 1;
+        for(int i=1; i<=n-1;i++) {
+            ans += 4*i;
+        }
+
+        return ans;
+
+        // long gridSize = 2*n-1;
+        // long halfGridSize = gridSize / 2 + 1;
+        // return gridSize*gridSize - 2*(halfGridSize*halfGridSize - halfGridSize);
     }
 }
