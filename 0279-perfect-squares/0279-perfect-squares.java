0b43ff04e67ce82f778f50c7302958c9341588a0
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-03-22 18:21:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 0279-perfect-squares/0279-perfect-squares.java
+++ 0279-perfect-squares/0279-perfect-squares.java
@@ -1,9 +1,43 @@
 class Solution {
     public int numSquares(int n) {
 
-        int[] dp = new int[n+1];
-        Arrays.fill(dp, -1);
-        return sol1(n, dp);
+        // int[] dp = new int[n+1];
+        // Arrays.fill(dp, -1);
+        // return sol1(n, dp);
+
+        return sol2(n);
+    }
+
+    // maths -> using lagrenge and legendres theorem
+    // ans will range from 1 to 4
+    public int sol2(int n) {
+        int sqrtVal = (int) Math.sqrt(n);
+
+        // if ans == 1, then n is perfect square
+        if(sqrtVal * sqrtVal == n)
+            return 1;
+        
+        // if ans == 4, then n = 4^k * (8*m + 7)
+        int N = n;
+        while(N > 0) {
+            N = N / 4;
+        }
+
+        if(N % 8 == 7)
+            return 4;
+
+        // if ans == 2, then i*i*j*j == n
+        for(int i=1; i*i <= n; i++) {
+            int sqr1 = i*i;
+            int ele2 = (int) Math.sqrt(n - sqr1);
+
+            if(ele2 * ele2 == (n - sqr1)) {
+                return 2;
+            }
+        }
+
+        // return 3
+        return 3;
     }
 
     public int sol1(int n, int[] dp) {
