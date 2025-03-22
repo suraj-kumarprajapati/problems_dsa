8ec2f3296fd61d17faea790f4e15458a1e513bab
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-03-22 17:34:41 +0530

    Time: 0 ms (100%), Space: 40.3 MB (93.78%) - LeetHub

--- 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
+++ 1823-find-the-winner-of-the-circular-game/1823-find-the-winner-of-the-circular-game.java
@@ -1,5 +1,19 @@
 class Solution {
     public int findTheWinner(int n, int k) {
+        // return sol1(n, k);
+
+        return sol2(n, k) + 1;
+    }
+
+    public int sol2(int n, int k) {
+        if(n == 1)
+            return 0;
+
+        return (sol2(n-1, k) + k) % n;
+    }
+
+    // time -> O(n*k), space -> O(n)
+    public int sol1(int n, int k) {
         Queue<Integer> q = new LinkedList<>();
 
         for(int i=1; i<=n; i++) {
