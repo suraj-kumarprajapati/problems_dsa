b5c725c9de4e7284d67e4312287d9693c5b05629
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-03-24 17:39:37 +0530

    Time: 0 ms (100%), Space: 40.6 MB (46.48%) - LeetHub

--- 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
+++ 1492-the-kth-factor-of-n/1492-the-kth-factor-of-n.java
@@ -3,6 +3,24 @@ class Solution {
         return sol1(n, k);
     }
 
+    public int sol2(int n, int k) {
+        for(int i=1; i<=(int) Math.sqrt(n); i++) {
+            if(n % i == 0 && --k == 0) 
+                return i;
+        }
+
+        for(int i=(int) Math.sqrt(n); i>=1; i--) {
+
+            if(i * i == n)
+                continue;
+
+            if(n % i == 0 && --k == 0)
+                return n/i;
+        }
+
+        return -1;
+    }
+
     public int sol1(int n, int k) {
         for(int i=1; i<=n/2; i++) {
             if(n % i == 0 && --k == 0) {
