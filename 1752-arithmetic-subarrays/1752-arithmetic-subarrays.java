9bb96637c919fb7fd32eb3a0830bfddf8f89f8db
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-03-25 19:19:21 +0530

    Time: 54 ms (8.5%), Space: 45.4 MB (78.75%) - LeetHub

--- 1752-arithmetic-subarrays/1752-arithmetic-subarrays.java
+++ 1752-arithmetic-subarrays/1752-arithmetic-subarrays.java
@@ -7,24 +7,39 @@ class Solution {
         for(int i=0; i<m; i++) {
             int left = l[i];
             int right = r[i];
+            int len = right - left + 1;
+            Map<Integer, Integer> freq = new HashMap<>();
 
-
-            // Extract and sort the subarray
-            int[] subArray = Arrays.copyOfRange(nums, left, right + 1);
-            Arrays.sort(subArray);
-
-            // Check for arithmetic sequence
-            boolean isArithmetic = true;
-            int diff = subArray[1] - subArray[0];
-
-            for (int j = 2; j < subArray.length; j++) {
-                if (subArray[j] - subArray[j - 1] != diff) {
-                    isArithmetic = false;
-                    break;
-                }
+            int min = Integer.MAX_VALUE;
+            int max = Integer.MIN_VALUE;
+            for(int j=left; j<=right; j++) {
+                freq.put(nums[j], freq.getOrDefault(nums[j], 0) + 1);
+                min = Math.min(min, nums[j]);
+                max = Math.max(max, nums[j]);
             }
 
-            ans.add(isArithmetic);
+            if((max-min) % (len-1) != 0) {
+                ans.add(false);
+                continue;
+            }
+
+            int d = (max - min) / (len-1);
+
+            boolean currAns = true;
+            for(int j=0; j<=len-1; j++) {
+                int next = min + j*d;
+
+                if(!freq.containsKey(next)) {
+                    currAns = false;
+                    break;
+                }
+
+                freq.put(next, freq.get(next) - 1);
+                if(freq.get(next) == 0)
+                    freq.remove(next);
+            }
+
+            ans.add(currAns);
         }
 
         return ans;
