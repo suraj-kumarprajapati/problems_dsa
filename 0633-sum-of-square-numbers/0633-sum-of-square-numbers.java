2ba2169f50beade4221eb1e4549a80a53992393a
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-03-26 13:44:31 +0530

    Time: 4 ms (60.11%), Space: 40.4 MB (79.3%) - LeetHub

--- 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -1,8 +1,13 @@
 class Solution {
     public boolean judgeSquareSum(int c) {
+        // return sol1(c);
 
-       
+        return sol2(c);
+        
+    }
 
+    // time -> O(sqrt(c) * log(c))
+    public boolean sol1(int c) {
         for(int i=0; i<=(int)Math.sqrt(c); i++) {
             int sqr1 = i*i;
             int num2 = (int)Math.sqrt(c - sqr1);
@@ -13,4 +18,25 @@ class Solution {
 
         return false;
     }
+
+    public boolean sol2(int c) {
+        long b = (long) Math.sqrt(c);
+        long a = 0;
+
+        while(a <= b) {
+            long res = a*a + b*b ;
+            if(res == c)
+                return true;
+            else if(res < c) {
+                a += 1;
+            }
+            else {
+                b -= 1;
+            }
+        }
+
+        return false;
+    }
+
+
 }
