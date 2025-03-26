ff05502a0663312fb430fb5959f421b33fdc5e71
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-03-26 13:08:46 +0530

    Time: 0 ms (100%), Space: 40.3 MB (90.27%) - LeetHub

--- 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
+++ 2579-count-total-number-of-colored-cells/2579-count-total-number-of-colored-cells.java
@@ -1,15 +1,6 @@
 class Solution {
     public long coloredCells(int n) {
-
-        long ans = 1;
-        for(int i=1; i<=n-1;i++) {
-            ans += 4*i;
-        }
-
-        return ans;
-
-        // long gridSize = 2*n-1;
-        // long halfGridSize = gridSize / 2 + 1;
-        // return gridSize*gridSize - 2*(halfGridSize*halfGridSize - halfGridSize);
+        long N = (long) n;
+        return (long) (1 + (N-1)*(2*4 + (N-2)*4)/2);
     }
 }
