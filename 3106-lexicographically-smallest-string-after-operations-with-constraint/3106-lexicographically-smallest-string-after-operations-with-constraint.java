a6ae2cef985eab8c518cc04c9dae74208f17b54e
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-03-27 16:03:39 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3106-lexicographically-smallest-string-after-operations-with-constraint/3106-lexicographically-smallest-string-after-operations-with-constraint.java
@@ -0,0 +1,44 @@
+class Solution {
+    public String getSmallestString(String s, int k) {
+        StringBuilder str = new StringBuilder();
+
+        int n = s.length();
+        for(int i=0; i<n; i++) {
+            char ch = s.charAt(i);
+
+            // go right side
+            int min1 = (int) (ch - 'a');
+            int dist1 = 0;
+            for(int j=1; j<=Math.min(26, k); j++) {
+                int ind = ( ch - 'a' + j) % 26;
+                if(ind < min1) {
+                    dist1 = j;
+                    min1 = Math.min(min1, ind);
+                }
+            }
+
+            // go left side
+            int dist2 = 0;
+            int min2 = (int) (ch - 'a');
+            for(int j=1; j<=Math.min(26, k); j++) {
+                int ind = (ch - 'a' + 26 - j) % 26;
+                if(ind < min2) {
+                    dist2 = j;
+                    min2 = Math.min(min2, ind);
+                }
+            }
+
+            
+            if(min1 < min2) {
+                k = k - dist1;
+                str.append((char) (min1 + 'a'));
+            }
+            else {
+                k = k - dist2;
+                str.append((char) (min2 + 'a'));
+            }
+        }
+
+        return str.toString();
+    }
+}
