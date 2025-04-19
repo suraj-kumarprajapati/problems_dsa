001d0510b6f9ce92c8f3808799d85e759521d5a3
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-04-19 22:44:28 +0530

    Time: 74 ms (5.85%), Space: 57.6 MB (21.34%) - LeetHub

---
+++ 2563-count-the-number-of-fair-pairs/2563-count-the-number-of-fair-pairs.java
@@ -0,0 +1,92 @@
+class Solution {
+    public long countFairPairs(int[] nums, int lower, int upper) {
+        // return sol1(nums, lower, upper);
+
+        return sol2(nums, lower, upper);
+    }
+
+    public long sol2(int[] nums, int lower, int upper) {
+        int n = nums.length;
+
+        Arrays.sort(nums);
+        
+        long count = 0;
+        for(int i=0; i<n; i++) {
+            int curr = nums[i];
+
+            int lowerInd = findLowerInd(nums, curr, lower);
+            int upperInd = findUpperInd(nums, curr, upper);
+
+            if(lowerInd != -1 && upperInd != -1 && lowerInd <= upperInd) {
+                count += (long) (upperInd - lowerInd + 1);
+                if(i >= lowerInd && i <= upperInd)
+                    count -= 1;
+            }
+        }
+
+        return count / 2;
+    }
+
+    private int findLowerInd(int[] nums, int curr, int lower) {
+        int n = nums.length;
+
+        int l = 0;
+        int u = n-1;
+        int ans = -1;
+        while(l <= u) {
+            int mid = l + (u - l) / 2;
+            int sum = curr + nums[mid];
+
+            if( sum >= lower) {
+                ans = mid;
+                u = mid - 1;
+            }   
+            else {
+                l = mid + 1;
+            }
+        }
+
+        return ans;
+    }
+
+
+    private int findUpperInd(int[] nums, int curr, int upper) {
+        int n = nums.length;
+
+        int l = 0;
+        int u = n-1;
+        int ans = -1;
+        while(l <= u) {
+            int mid = l + (u - l) / 2;
+            int sum = curr + nums[mid];
+
+            if( sum <= upper) {
+                ans = mid;
+                l = mid + 1;
+            }   
+            else {
+                u = mid - 1;
+            }
+        }
+
+        return ans;
+    }
+
+
+    // brute force approach
+    public long sol1(int[] nums, int lower, int upper) {
+        int n = nums.length;
+        long count = 0;
+
+        for(int i = 0; i <= n-2; i++) {
+            for(int j = i + 1; j < n; j++) {
+                int sum = nums[i] + nums[j];
+                if(sum >= lower && sum <= upper) {
+                    count += 1;
+                }
+            }
+        }
+
+        return count;
+    }
+}
