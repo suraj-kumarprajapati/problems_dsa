4838a9cc1031f3b6fbbefa28afc1582dfb011e33
Suraj Prajapati <122683284+suraj-kumarprajapati@users.noreply.github.com>
2025-04-21 17:49:55 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

--- 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.java
+++ 2145-count-the-hidden-sequences/2145-count-the-hidden-sequences.java
@@ -1,19 +1,17 @@
 class Solution {
     public int numberOfArrays(int[] differences, int lower, int upper) {
-        int n = differences.length;
-
-        int minCumm = 0;
+         int minCumm = 0;
         int maxCumm = 0;
         int cumm = 0;
         for(int diff : differences) {
             cumm += diff;
             minCumm = Math.min(minCumm, cumm);
             maxCumm = Math.max(maxCumm, cumm);
-
-            if(maxCumm - minCumm > upper - lower)
-                return 0;
         }
 
-        return  upper - (maxCumm - minCumm) - lower + 1;
+        int minStartPoint = lower - minCumm;
+        int maxStartPoint = upper - maxCumm;
+
+        return minStartPoint < maxStartPoint ? maxStartPoint - minStartPoint + 1 : 0;
     }
 }
